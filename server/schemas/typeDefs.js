9963452694a8803fe69c04956664ec4d97aa42f0
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2024-03-05 13:10:11 -0800

    Revert "Merge pull request #11 from Ivana-Djordjevic/models"
    This reverts commit d3bab889eee7e2eed9b55fb1cb9a57b5c58336aa, reversing
    changes made to 315a9445525ab99cfe55660522d8f59a7461cc34.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,20 +5,6 @@ const typeDefs = `
         email: String!
         password: String!
     }
-    type Post {
-        _id: ID
-        caption: String
-        altText: String!
-        image: String!
-        user: User!
-        comments: [Comment]
-    }
-    type Comment {
-        _id: ID
-        commentText: String!
-        user: User!
-        post: Post!
-    }
 
     type Auth {
         token: ID!
@@ -27,28 +13,11 @@ const typeDefs = `
 
     type Query {
         me: User
-        users(username: String): [User]
-        posts(username: String): [Post]
-        comments(username: String): [Comment]
     }
 
     type Mutation {
-        
-        // User mutations
-        login(email: String!, password: String!): Auth
         createUser(username: String!, email: String!, password: String!): Auth
-        editUser(username: String, email: String, password: String): User
-        deleteUser(_id: ID!): User
-
-        // Post mutations
-        createPost(caption: String, altText: String!, image: String!): Post
-        editPost(_id: ID!, caption: String, altText: String, image: String): Post
-        deletePost(_id: ID!): Post
-
-        // Comment mutations
-        createComment(postId: ID!, commentText: String!): Comment
-        editComment(_id: ID!, commentText: String!): Comment
-        deleteComment(_id: ID!): Comment
+        login(email: String!, password: String!): Auth
     }
 `;
 
